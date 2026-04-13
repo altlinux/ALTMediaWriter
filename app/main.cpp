c3e0ccc6d9213c11c91506ce10b1e3792131c9f7
Maria Alexeeva <alxvmr@altlinux.org>
2026-04-13 14:37:20 +0400

    feat: new window icon

--- app/main.cpp
+++ app/main.cpp
@@ -105,7 +105,7 @@ int main(int argc, char **argv) {
 
     qDebug() << "Application constructed";
 
-    app.setWindowIcon(QIcon(":/mediawriter.ico"));
+    app.setWindowIcon(QIcon(":/mediawriter.svg"));
 
     QTranslator translator;
     translator.load(QLocale(QLocale().language(), QLocale().country()), QString(), QString(), ":/translations");
